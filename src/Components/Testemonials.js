dbca5606a0bb106ae83866adc275bfa24426aa18
vilson junior <vilsonrjunior@gmail.com>
2019-09-02 14:39:58 +0100

    changes to testemonial cards

--- src/Components/Testemonials.js
+++ src/Components/Testemonials.js
@@ -1,26 +1,35 @@
 import React from "react";
-import Slider from "react-slick";
-import { Card } from "antd";
-import "antd/dist/antd.css";
+import Slider from "react-slick"
+
+
+function Arrows(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, background: "grey", borderRadius: '50%' }}
+      onClick={onClick}
+    />
+  );
+}
 
 class Testemonials extends React.Component {
   render() {
-
     let settings = {
       dots: true,
-      infinite: false,
-      autoplay: true,
       infinite: true,
-      speed: 2000,
+      speed: 500,
       slidesToShow: 3,
-      slidesToScroll: 4,
+      slidesToScroll: 3,
       initialSlide: 0,
+      nextArrow: <Arrows />,
+      prevArrow: <Arrows />,
       responsive: [
         {
           breakpoint: 1024,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 3,
+            slidesToScroll: 2,
             infinite: true,
             dots: true
           }
@@ -29,7 +38,7 @@ class Testemonials extends React.Component {
           breakpoint: 600,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 2,
+            slidesToScroll: 1,
             initialSlide: 2
           }
         },
@@ -60,85 +69,57 @@ class Testemonials extends React.Component {
     }
 
     const divStyle={
-      backgroundColor: '#fff',
+      // backgroundColor: '#fff',
       textAlign: 'center',
       paddingTop: '4%',
-      paddingBottom: '6%',
-      paddingRight: '2%',
-      paddingLeft: '8%'
+      paddingBottom: '4%',
+      paddingRight: '4%',
+      paddingLeft: '4%',
+      backgroundColor: '#fff'
     }
 
-    const { Meta } = Card;
+    const h3Style={
+      backgroundColor: '#fff',
+      borderRadius: '2%',
+      boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+      color: '#1d809f',
+      fontSize: '36px',
+      lineHeight: '680px',
+      height: '400px',
+      margin: '10px',
+      padding: '2%',
+      position: 'relative',
+      textAlign: 'center'
+    }
 
     return (
       <div style={divStyle}>
+
         <h5 style={h5Style}>TESTEMONIALS</h5>
         <h2 style={h2Style}>What do former students have to say about UniEnglish?</h2>
         <Slider {...settings}>
-        <div>
-           <Card
-             hoverable
-             style={{ width: 270, height: 350 }}
-             cover={<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>}
-             >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-           </Card>
-        </div>
-        <div>
-           <Card
-             hoverable
-             style={{ width: 270, height: 350 }}
-             cover={<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>}
-             >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-           </Card>
-        </div>
-        <div>
-           <Card
-             hoverable
-             style={{ width: 270, height: 350 }}
-             cover={<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>}
-             >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-           </Card>
-        </div>
-        <div>
-           <Card
-             hoverable
-             style={{ width: 270, height: 350 }}
-             cover={<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>}
-             >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-           </Card>
-        </div>
-        <div>
-           <Card
-             hoverable
-             style={{ width: 270, height: 350 }}
-             cover={<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>}
-             >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-           </Card>
-        </div>
-        <div>
-           <Card
-             hoverable
-             style={{ width: 270, height: 350 }}
-             cover={<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>}
-             >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-           </Card>
-        </div>
-        <div>
-           <Card
-             hoverable
-             style={{ width: 270, height: 350 }}
-             cover={<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>}
-             >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-           </Card>
-        </div>
-      </Slider>
+
+          <div>
+            <h3 style={h3Style}>2</h3>
+          </div>
+
+          <div>
+            <h3 style={h3Style}>3</h3>
+          </div>
+
+          <div>
+            <h3 style={h3Style}>4</h3>
+          </div>
+
+          <div>
+            <h3 style={h3Style}>5</h3>
+          </div>
+
+          <div>
+            <h3 style={h3Style}>6</h3>
+          </div>
+
+        </Slider>
       </div>
     );
   }
